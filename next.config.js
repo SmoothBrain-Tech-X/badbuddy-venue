8540981a74d9b520cca4803676da69586ba73e8c
Teerut26 <teerutsr@gmail.com>
2024-11-04 02:53:52 +0700

    Update Next.js configuration to set output to standalone and format environment variables

--- next.config.js
+++ next.config.js
@@ -6,9 +6,11 @@ import "./src/env.js";
 
 /** @type {import("next").NextConfig} */
 const config = {
-    env: {
-        NEXT_PUBLIC_BASE_API: "https://general-badbuddy-be.tu4rl4.easypanel.host/api"
-    }
+  env: {
+    NEXT_PUBLIC_BASE_API:
+      "https://general-badbuddy-be.tu4rl4.easypanel.host/api",
+  },
+  output: "standalone",
 };
 
 export default config;
